c8fef875c5776862c73f0aa13f4bf577b36d0ecb
LCoghlan1 <luke.coghlan@hotmail.com>
2021-03-18 10:54:21 +0000

    mix migration for heroku upload

--- db/migrate/20210309200349_create_absences.rb
+++ db/migrate/20210309200349_create_absences.rb
@@ -2,8 +2,8 @@ class CreateAbsences < ActiveRecord::Migration[6.1]
   def change
     create_table :absences do |t|
       t.date :date
-      t.boolean :full_pay
-      t.boolean :half_pay
+      t.float :full_pay
+      t.float :half_pay
       t.text :description
       t.references :employee, null: false, foreign_key: true
 
--- db/migrate/20210313123734_change_pay_columns_in_absences_to_float.rb
+++
@@ -1,6 +0,0 @@
-class ChangePayColumnsInAbsencesToFloat < ActiveRecord::Migration[6.1]
-  def change
-    change_column :absences, :half_pay, :float
-    change_column :absences, :full_pay, :float
-  end
-end
